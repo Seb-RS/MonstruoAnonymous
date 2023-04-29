9a42e7dc56358f9ea79708bb6e6a0ab327aea26a
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 22:46:22 -0400

    style: added opacity class

--- resources/js/Components/MacTerminal.vue
+++ resources/js/Components/MacTerminal.vue
@@ -7,7 +7,7 @@ export default {
 </script>
 
 <template>
-  <div class="rotateAX absolute flex mt-10 inset-0 m-auto text-white">
+  <div class="rotateAX absolute flex mt-10 inset-0 m-auto text-white opacity-70">
     <div class="md:w-1/2 mx-auto">
       <div
         class="w-full shadow-2xl subpixel-antialiased rounded h-[90%] bg-black border-black mx-auto"
