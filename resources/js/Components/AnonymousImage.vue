74bacaf139a289031a0144022fbc5612b66c8344
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-29 03:02:53 -0400

    feat: added PowerGlitch effect on image

--- resources/js/Components/AnonymousImage.vue
+++ resources/js/Components/AnonymousImage.vue
@@ -1,7 +1,21 @@
 <template>
   <img
-    class="absolute inset-0 m-auto opacity-80 w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+    class="absolute inset-0 m-auto opacity-80 w-[200px] h-[200px] md:w-[400px] md:h-[400px] glitch"
     src="/storage/assets/anonymous.png"
     alt="anonymous"
   />
 </template>
+
+<script>
+import { PowerGlitch } from "powerglitch";
+export default {
+  mounted() {
+    PowerGlitch.glitch(".glitch", {
+      timing: {
+        duration: 4000,
+        easing: "ease-in-out",
+      },
+    });
+  },
+};
+</script>
