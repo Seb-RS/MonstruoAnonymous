0030c6e207daff9bef50ee24658fe01296cf712b
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 23:08:32 -0400

    feat: imported CodingImage component on App.vue

--- resources/js/Pages/Home/Show.vue
+++ resources/js/Pages/Home/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MacTerminal from "@/components/MacTerminal.vue";
 import AnonymousImage from "../../Components/AnonymousImage.vue";
+import CodingImage from "@/Components/CodingImage.vue";
 import { defineProps } from 'vue';
 
 const props = defineProps({
@@ -13,6 +14,7 @@ const props = defineProps({
 
 <template>
   <div class="relative bg-black/95 h-screen w-screen overflow-hidden">
+    <CodingImage/>
     <MacTerminal :clientIP="clientIP"/>
     <AnonymousImage/>
   </div>
