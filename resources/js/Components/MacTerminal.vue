46a583cbf33e76ec6c8dc9c8d0fb9a3c6819e4fa
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 23:54:23 -0400

    feat: added ping functions

--- resources/js/Components/MacTerminal.vue
+++ resources/js/Components/MacTerminal.vue
@@ -3,6 +3,28 @@ export default {
   props: {
     clientIP: String,
   },
+  data() {
+    return {
+      pingList: [],
+    };
+  },
+  created() {
+    this.pingList.push("64 bytes from " + this.clientIP + " icmp_seq=1 ttl=242 time=");
+  },
+  methods: {
+    addPing() {
+      this.pingList.push(
+        "64 bytes from " +
+          this.clientIP +
+          " icmp_seq=" +
+          (this.pingList.length + 1) +
+          " ttl=242 time="
+      );
+    },
+  },
+  mounted() {
+    setInterval(this.addPing, 1000); // Agregar un nuevo ping cada 3 segundos
+  },
 };
 </script>
 
@@ -10,7 +32,7 @@ export default {
   <div class="rotateAX absolute flex mt-10 inset-0 m-auto text-white opacity-70">
     <div class="md:w-1/2 mx-auto">
       <div
-        class="w-full shadow-2xl subpixel-antialiased rounded h-[90%] bg-black border-black mx-auto"
+        class="w-full shadow-2xl subpixel-antialiased rounded max-h-[90%] h-[90%] bg-black border-black mx-auto"
       >
         <div
           class="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black"
@@ -32,21 +54,11 @@ export default {
             <p class="text-center text-xs">sebdev — ping {{ clientIP }} — 109x31</p>
           </div>
         </div>
-        <div class="pl-1 pt-1 h-auto text-white font-mono text-xs bg-black" id="console">
+        <div class="pl-1 pt-1 text-white font-mono text-xs h-[95%] overflow-y-auto" id="console">
           <p class="pb-1">Last login: Wed Sep 25 09:11:04 on ttys002</p>
-          <p class="pb-1">
-            Monstruo-Servers-de-Seb-2:~ sebdev$ ping
-            {{ clientIP }}
-          </p>
-          <p class="pb-1">
-            PING
-            {{ clientIP }}: 56 data bytes
-          </p>
-          <p class="pb-1">
-            64 bytes from
-            {{ clientIP }}
-            icmp_seq=1 ttl=242 time=<!-- ms float -->
-            ms
+          <p class="pb-1">Monstruo-Servers-de-Seb-2:~ sebdev$ ping {{ clientIP }}</p>
+          <p v-for="(ping, index) in pingList" :key="index" class="pb-1">
+            {{ ping }}
           </p>
         </div>
       </div>
