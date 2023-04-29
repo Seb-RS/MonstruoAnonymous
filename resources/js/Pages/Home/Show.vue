0eb127cdea530b765fa17d382ee90b4ca5f542d6
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 22:15:06 -0400

    feat(refactor): modified App.vue to use defineProps and pass the clientIP to the MacTerminal component

--- resources/js/Pages/Home/Show.vue
+++ resources/js/Pages/Home/Show.vue
@@ -1,5 +1,17 @@
+<script setup>
+import MacTerminal from "@/components/MacTerminal.vue";
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  clientIP: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
 <template>
-    <div>
-        
-    </div>
+  <div class="relative bg-black/95 h-screen w-screen">
+    <MacTerminal :clientIP="clientIP"/>
+  </div>
 </template>
