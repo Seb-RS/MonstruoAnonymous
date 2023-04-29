7e6c3578efa7651d15342ce28d036dfaafc3835c
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 22:45:21 -0400

    feat: created AnonymousImage component and added image on Storage

---
+++ resources/js/Components/AnonymousImage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute flex items-center justify-center inset-0 m-auto opacity-80">
+    <img class="w-[400px] h-[400px]" src="/storage/assets/anonymous.png" alt="anonymous">
+  </div>
+</template>
