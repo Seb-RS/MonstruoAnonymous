9ca36ac4f027f24ba964729d6725f578a8b94ae0
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-29 01:45:55 -0400

    refactor: removed div in AnonymousImage

--- resources/js/Components/AnonymousImage.vue
+++ resources/js/Components/AnonymousImage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="absolute flex items-center justify-center inset-0 m-auto opacity-80">
-    <img class="w-[200px] h-[200px] md:w-[400px] md:h-[400px]" src="/storage/assets/anonymous.png" alt="anonymous">
-  </div>
+  <img
+    class="absolute inset-0 m-auto opacity-80 w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+    src="/storage/assets/anonymous.png"
+    alt="anonymous"
+  />
 </template>
