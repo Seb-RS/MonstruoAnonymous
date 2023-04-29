68c42b708af0089310e0f397e0a2f3520f22f5c0
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-04-28 23:08:11 -0400

    feat: created CodingImage component and added bg-coding image on storage/assets dir

---
+++ resources/js/Components/CodingImage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="blur w-[80%] h-full bg-no-repeat bg-center bg-cover filter brightness-50" style="background-image: url('/storage/assets/bg-coding.png')"></div>
+</template>
+
+<style scoped>
+.blur {
+  filter: blur(3px);
+}
+</style>
